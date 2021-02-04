6b125a8561ae2a15f2b31559c9f3bbf19144d4e3
1kzpro <kazymizam@mail.ru>
2021-02-03 19:53:03 -0600

    yo yo

--- src/simple_serial.c
+++ src/simple_serial.c
@@ -9,11 +9,11 @@
 
 
 void* serial_read(void* ptr) {
-  int *serial_device = (int *)ptr;
+  int *fd = (int *)ptr;
   unsigned char z = 'D';
 
   while (rc_get_state() != EXITING) {
-    if (read(serial_device, &z, 1) > 0) {
+    if (read(fd, &z, 1) > 0) {
       printf("%c", z);
     }
   }
@@ -23,7 +23,7 @@ void* serial_read(void* ptr) {
 
 
 int simple_serial_init(struct simple_serial_t *serial_device) {
-    int fd = serial_open("/dev/tty" + serial_device->port, serial_device->baud_rate, 0);
+    int fd = serial_open(serial_device->port, serial_device->baud_rate, 0);
     if (fd == -1) {
       fprintf(stderr, "ERROR in opening serial\n");
         return -1;
@@ -31,7 +31,7 @@ int simple_serial_init(struct simple_serial_t *serial_device) {
 
     // start thread
     if (rc_pthread_create(
-          serial_device->serial_read_thread, &serial_read, (void *)&fd, SCHED_FIFO, INPUT_MANAGER_PRI) == -1) {
+          &serial_device->serial_read_thread, &serial_read, (void *)&fd, SCHED_FIFO, INPUT_MANAGER_PRI) == -1) {
         fprintf(stderr, "ERROR in simple_serial_init, failed to start thread\n");
         return -1;
     }
@@ -40,7 +40,7 @@ int simple_serial_init(struct simple_serial_t *serial_device) {
 }
 
 int simple_serial_cleanup(struct simple_serial_t *serial_device) {
-  if (rc_pthread_timed_join(serial_device->serial_read_thread, NULL, INPUT_MANAGER_TOUT) == 1)
+  if (rc_pthread_timed_join(&serial_device->serial_read_thread, NULL, INPUT_MANAGER_TOUT) == 1)
   {
     fprintf(stderr, "WARNING: in serialer_cleanup, thread join timeout\n");
     return -1;
