c0850987196605f2272be6375eda6a90ad8b7b21
1kzpro <kazymizam@mail.ru>
2021-02-03 19:59:08 -0600

    almost pls

--- src/simple_serial.c
+++ src/simple_serial.c
@@ -13,7 +13,7 @@ void* serial_read(void* ptr) {
   unsigned char z = 'D';
 
   while (rc_get_state() != EXITING) {
-    if (read(I(*fd, &z, 1) > 0) {
+    if (read((*fd, &z, 1) > 0) {
       printf("%c", z);
     }
   }
@@ -31,7 +31,7 @@ int simple_serial_init(struct simple_serial_t *serial_device) {
 
     // start thread
     if (rc_pthread_create(
-          *serial_device.serial_read_thread, &serial_read, (void *)&fd, SCHED_FIFO, INPUT_MANAGER_PRI) == -1) {
+          &(serial_device->serial_read_thread), &serial_read, (void *)&fd, SCHED_FIFO, INPUT_MANAGER_PRI) == -1) {
         fprintf(stderr, "ERROR in simple_serial_init, failed to start thread\n");
         return -1;
     }
@@ -40,7 +40,7 @@ int simple_serial_init(struct simple_serial_t *serial_device) {
 }
 
 int simple_serial_cleanup(struct simple_serial_t *serial_device) {
-  if (rc_pthread_timed_join(*serial_device.serial_read_thread, NULL, INPUT_MANAGER_TOUT) == 1)
+  if (rc_pthread_timed_join(&(serial_device->serial_read_thread), NULL, INPUT_MANAGER_TOUT) == 1)
   {
     fprintf(stderr, "WARNING: in serialer_cleanup, thread join timeout\n");
     return -1;
