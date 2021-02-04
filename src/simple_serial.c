922c5af5898983f37662668692ab093f913cead6
1kzpro <kazymizam@mail.ru>
2021-02-03 20:26:51 -0600

    lets check it out

--- src/simple_serial.c
+++ src/simple_serial.c
@@ -23,15 +23,15 @@ void* serial_read(void* ptr) {
 
 
 int simple_serial_init(struct simple_serial_t *serial_device) {
-    int fd = serial_open(serial_device->port, serial_device->baud_rate, 0);
-    if (fd == -1) {
+    int * fd = &serial_open(serial_device->port, serial_device->baud_rate, 0);
+    if (*fd == -1) {
       fprintf(stderr, "ERROR in opening serial\n");
         return -1;
     }
 
     // start thread
     if (rc_pthread_create(
-          &(serial_device->serial_read_thread), &serial_read, (void *)&fd, SCHED_FIFO, INPUT_MANAGER_PRI) == -1) {
+          &(serial_device->serial_read_thread), &serial_read, (void *)fd, SCHED_FIFO, INPUT_MANAGER_PRI) == -1) {
         fprintf(stderr, "ERROR in simple_serial_init, failed to start thread\n");
         return -1;
     }
